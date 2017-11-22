044bfd015093c73ece1095aa4241a4b880cd0964
FHLC <FHLC@FHLC-PC>
2017-11-22 13:23:27 -0200

    Teste 5

--- src/com/fhlc7/teste/Teste.java
+++ src/com/fhlc7/teste/Teste.java
@@ -6,5 +6,6 @@ public class Teste {
 		System.out.println("Outro Teste");
 		System.out.println("Mais um Teste");
 		System.out.println("Só Mais um Teste");
+		System.out.println("Deu certo");
 	}
 }
